33779670c6278afa04678cd89acfa1caf826b8a6
Jonah Walker <supposedlysam@gmail.com>
2024-09-24 12:27:14 -0400

    test: load and display presidents

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,20 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:learn_the_presidents/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Load and display presidents', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const LearnThePresidentsApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that the app loads and displays the presidents.
+    await tester.pumpAndSettle();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Check that the sections are displayed.
+    expect(find.text('Section 1'), findsOneWidget);
+    expect(find.text('Section 2'), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Check that the presidents are displayed.
+    expect(find.text('George Washington'), findsOneWidget);
+    expect(find.text('John Adams'), findsOneWidget);
   });
 }
